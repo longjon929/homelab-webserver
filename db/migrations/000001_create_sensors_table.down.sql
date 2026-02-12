c667f30eb2782e2b6bcb8c65d2c5d412e620f094
Jonathan Long <long.jonathan@gmail.com>
2026-02-11 23:00:02 -0500

    Add soil sensor

--- db/migrations/000001_create_sensors_table.down.sql
+++ db/migrations/000001_create_sensors_table.down.sql
@@ -1,5 +1,12 @@
 -- 1. Drop the transaction table (and its indexes) first
-DROP TABLE IF EXISTS readings;
+DROP TABLE IF EXISTS environment_data;
+DROP INDEX IF EXISTS idx_environment_data_sensor_time;
+DROP INDEX IF EXISTS idx_environment_data_location_time
+
+DROP TABLE IF EXISTS soil_data;
+DROP INDEX IF EXISTS idx_soil_sensor_time;
+DROP INDEX IF EXISTS idx_soil_location_time;
+
 
 -- 2. Drop the reference tables (order between these two doesn't matter)
 DROP TABLE IF EXISTS locations;
